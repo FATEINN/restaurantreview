43880dc93dd5e18235ac45809145edd93f2a44b4
Faith Salawe <40576436+FATEINN@users.noreply.github.com>
2018-11-02 09:17:26 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -84,7 +84,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: 'XXXXXXXXXX',
+    mapboxToken: 'pk.eyJ1IjoiZmFpdGhzYWx6IiwiYSI6ImNqbXQzZjYwaDAycmszbHA3OWJrMDZ6bHgifQ.r5BhHeGSD3ot-qv-2ZkBUg',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
