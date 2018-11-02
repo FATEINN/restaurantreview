712969fb8de02628d2302d8474d6c3e8599c9f1f
Faith Salawe <40576436+FATEINN@users.noreply.github.com>
2018-11-02 12:57:45 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,4 @@
-// for like CSS, HTML...
-const staticCacheName = 'restaurants-static';
-
-// for images.
-const contentImgsCache = 'restaurants-content-imgs';
-const allCaches = [
-    staticCacheName,
-    contentImgsCache
-];
-
-
-self.addEventListener('const staticCacheName = 'review-cache-v1';
+const staticCacheName = 'review-cache-v1';
 const assets = [
   './',
   'index.html',
@@ -39,6 +28,9 @@ const assets = [
   './img/8.jpg',
   './img/9.jpg',
   './img/10.jpg',
+  './img/favicon.png',
+  'https://fonts.googleapis.com/css?family=Barlow+Condensed:300,400,500,600',
+  'https://fonts.googleapis.com/css?family=Montserrat:600'
 ];
 
 self.addEventListener('install', function(event) {
@@ -76,66 +68,4 @@ self.addEventListener('fetch', (event) => {
         return response || fetch(event.request);
       })
   );
-});install', function (event) {
-    console.log('installed successfully');
-    console.log('location');
-    console.log(location);
-
-    // caching all static files
-    event.waitUntil(
-        caches.open(staticCacheName).then(function (cache) {
-            return cache.addAll([
-                '/',
-                './css/styles.css',
-                './js/dbhelper.js',
-                './js/main.js',
-                './js/restaurant_info.js',
-                './index.html',
-                './restaurant.html'
-            ]);
-        })
-    );
-});
-
-self.addEventListener('activate', function (event) {
-    console.log('Activated successfully...');
-
-
-    event.waitUntil(
-        caches.keys().then(function (cacheNames) {
-            return Promise.all(
-                cacheNames.filter(function (cacheName) {
-                    return cacheName.startsWith('restaurants-') &&
-                        !allCaches.includes(cacheName);
-                }).map(function (cacheName) {
-                    return caches.delete(cacheName);
-                })
-            );
-        })
-    );
-});
-
-self.addEventListener('fetch', function (event) {
-    const requestUrl = new URL(event.request.url);
-    console.log(requestUrl);
-
-
-    if (requestUrl.origin === location.origin) {
-        if (requestUrl.pathname === '/') {
-            event.respondWith(caches.match('/'));
-            return;
-        }
-
-       /* if (requestUrl.pathname.startsWith('/photos/')) {
-            event.respondWith(servePhoto(event.request));
-            return;
-        }*/
-
-    }
-
-    event.respondWith(
-        caches.match(event.request).then(function (response) {
-            return response || fetch(event.request);
-        })
-    );
 });
